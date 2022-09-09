39dd878b8355d6f05e46e1771d794d87c0645f11
Pahul-Sidhu <97401471+Pahul-Sidhu@users.noreply.github.com>
2022-09-09 15:02:03 -0700

    installed plugins and fixed checkstyle errors

---
+++ src/main/java/comp/Mymethod.java
@@ -0,0 +1,14 @@
+package comp;
+
+/**
+ * <p>This class prints a greeting.</p>
+ *
+ * @author Pahul_Sidhu
+ * @version 17.0.0
+ */
+
+public class Mymethod {
+  public static void main(String[] args) {
+    System.out.println("Hello, My favourite animal is lion");
+  }
+}
--- src/main/java/comp2522/mymethod.java
+++
@@ -1,7 +0,0 @@
-package comp2522;
-
-public class mymethod {
-    public static void main(String[] args) {
-        System.out.println("Hello, My favourite animal is lion");
-    }
-}
